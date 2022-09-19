257ada14d64bf52fd402e9eca022f3f2eb255249
Yannick G <yannick013@gmail.com>
2022-09-19 23:56:56 +0200

    Removed useless echo

--- flac-to-opus.sh
+++ flac-to-opus.sh
@@ -6,7 +6,5 @@ find "/mnt/l/Music" -type f -name "*.flac" -print0 | while read -d $'\0' e;do
   if [ ! -f "$file.opus" ];then
     opusenc --comp 10 --bitrate 192 --vbr "$e" "${e%.*}.opus"
   fi
-  echo "$file"
 done
-
 exit 0
